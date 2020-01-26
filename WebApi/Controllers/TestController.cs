1b3b204c7bdbce8c9ab25c818350fb48a173dadf
Olivier CAGLIULI <oc91370@gmail.com>
2020-01-26 15:58:36 +0100

    Add some test files

---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get ()
+        {
+            return Ok();
+        }
+	}
+}
