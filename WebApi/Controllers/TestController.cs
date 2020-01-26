b89a510b94b0d1f7425ea0709a7d44143e456734
Olivier CAGLIULI <oc91370@gmail.com>
2020-01-26 22:22:28 +0100

    Add Swagger

--- WebApi/Controllers/TestController.cs
+++ WebApi/Controllers/TestController.cs
@@ -1,16 +1,21 @@
 ﻿using AutoMapper;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
 using WebApi.Entities;
 using WebApi.Models;
 
 namespace WebApi.Controllers
 {
-    [RoutePrefix("api/test")]
+    /// <summary>
+    /// Test controller
+    /// </summary>
+    [RoutePrefix("api/values")]
     public class TestController : ApiController
     {
         private readonly IMapper _mapper;
@@ -20,12 +25,27 @@ namespace WebApi.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Get all values
+        /// </summary>
+        /// <remarks>
+        /// Get the list of all values
+        /// </remarks>
+        /// <returns>A list of values</returns>
+        /// <response code="200">Returns the list of values</response>
+        /// <response code="404">Not found</response>
+        /// <response code="500">When an internal error occured</response>
         [Route()]
+        [HttpGet()]
+        [ResponseType(typeof(IEnumerable<TestModel>))]
         public IHttpActionResult Get()
         {
             try
             {
                 var result = new List<TestEntity>() { new TestEntity { FirstName = "Olivier", LastName = "CAGLIULI" } };
+                if (!result?.Any() ?? false)
+                    return NotFound();
+
                 var mappedResult = _mapper.Map<IEnumerable<TestModel>>(result);
 
                 return Ok(mappedResult);
--- WebApi/Entities/TestEntity.cs
+++ WebApi/Entities/TestEntity.cs
@@ -6,12 +6,24 @@ using System.Threading.Tasks;
 
 namespace WebApi.Entities
 {
+    /// <summary>
+    /// TestEntity
+    /// </summary>
     public class TestEntity
     {
+        /// <summary>
+        /// Id
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// First name
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last name
+        /// </summary>
         public string LastName { get; set; }
     }
 }
